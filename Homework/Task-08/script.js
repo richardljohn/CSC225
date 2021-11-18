cb209a66a5f0cd7492df5989eaf0711294fe3940
richardljohn <richardljohn@gmail.com>
2021-11-18 16:51:38 -0500

    Almost Done.

--- Homework/Task-08/script.js
+++ Homework/Task-08/script.js
@@ -15,15 +15,16 @@ function reverseThisString(string){
 // argument: Hello World
 // return: hELLO wORLD
 function swapCase(string) {
+    let new_string = ""
     for(let i = 0; i < string.length; i++){
         if(string[i] === string[i].toLowerCase()){
-            string[i] = string[i].toUpperCase();
+            new_string += string[i].toUpperCase();
         }
         else { 
-            string[i] = string[i].toLowerCase()
+            new_string += string[i].toLowerCase()
         }
     }
-    return string; 
+    return new_string; 
 }
 
 //convert array of numbers from farenheit to celcius
@@ -63,7 +64,14 @@ function passOrFail(array){
 function germanNumbers(){
     const cardinalNumbers = [2,3,4,5,6];
     const germanNumbers = ['zwei', 'drei', 'vier', 'fünf', 'sechs'];
-
+    let numbers = [];
+    numbers = cardinalNumbers.forEach(function(num, i){
+        let message = '';
+        message += num;
+        message += ' is ';
+        message += germanNumbers[i];
+    })
+    return numbers;
 }
 
 
@@ -73,7 +81,18 @@ function germanNumbers(){
 // hint use: Array.filter
 function returnPrimeNumbers(){
     const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-
+    let primes = numbers.filter(function(num){
+        if(num == 2){
+            return true;
+        }
+        for(let i = 2; i < num; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    })
+    return primes;
 }
 
 //Write a function that loops through and console.log's the numbers from 1 to 100, except multiples of three, log (without quotes) "CSC225 RULES" instead of the number, for the multiples of five, log (without quotes) "I LOVE JAVASCRIPT". For numbers which are multiples of both three and five, log (without quotes) "CSC225 RULES I LOVE JAVASCRIPT" Console log out:
@@ -98,3 +117,5 @@ console.log(reverseThisString("Hello"))
 console.log(swapCase("Hello, WOrlD"))
 console.log(toCelcius([23, 32, 41, 50, 59]))
 console.log(passOrFail([20, 30, 50, 80, 90, 100]))
+console.log(germanNumbers())
+console.log(returnPrimeNumbers())
