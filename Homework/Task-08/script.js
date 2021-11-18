8759e439801cb0b1cad9b7c5586fcca89fe4c81d
richardljohn <richardljohn@gmail.com>
2021-11-18 17:02:20 -0500

    Almost Almost

--- Homework/Task-08/script.js
+++ Homework/Task-08/script.js
@@ -65,12 +65,13 @@ function germanNumbers(){
     const cardinalNumbers = [2,3,4,5,6];
     const germanNumbers = ['zwei', 'drei', 'vier', 'fünf', 'sechs'];
     let numbers = [];
-    numbers = cardinalNumbers.forEach(function(num, i){
-        let message = '';
-        message += num;
-        message += ' is ';
-        message += germanNumbers[i];
-    })
+    cardinalNumbers.forEach(num => {
+        germanNumbers.forEach(word => {
+            numbers += num;
+            numbers += " is ";
+            numbers += word;
+        });
+    });
     return numbers;
 }
 
@@ -79,21 +80,21 @@ function germanNumbers(){
 // example: 
 // return [2, 3, 5, 7, 11, 13]
 // hint use: Array.filter
-function returnPrimeNumbers(){
-    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-    let primes = numbers.filter(function(num){
-        if(num == 2){
-            return true;
-        }
-        for(let i = 2; i < num; i++){
-            if(num % i == 0){
-                return false;
-            }
-        }
-        return true;
-    })
-    return primes;
-}
+// function returnPrimeNumbers(){
+//     const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+//     let primes = numbers.filter(function(num){
+//         if(num == 2){
+//             return true;
+//         }
+//         for(let i = 2; i < num; i++){
+//             if(num % i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     });
+//     return primes;
+// }
 
 //Write a function that loops through and console.log's the numbers from 1 to 100, except multiples of three, log (without quotes) "CSC225 RULES" instead of the number, for the multiples of five, log (without quotes) "I LOVE JAVASCRIPT". For numbers which are multiples of both three and five, log (without quotes) "CSC225 RULES I LOVE JAVASCRIPT" Console log out:
 function printer(){
@@ -118,4 +119,4 @@ console.log(swapCase("Hello, WOrlD"))
 console.log(toCelcius([23, 32, 41, 50, 59]))
 console.log(passOrFail([20, 30, 50, 80, 90, 100]))
 console.log(germanNumbers())
-console.log(returnPrimeNumbers())
+//console.log(returnPrimeNumbers())
